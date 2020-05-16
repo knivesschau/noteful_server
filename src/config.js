0079572a6dbd1d5703426432899b5d27740a756a
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-05-16 13:48:35 -0700

    preparing for deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://catrinafuentes@localhost/noteful_api',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://catrinafuentes@localhost/noteful_test_api'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://catrinafuentes@localhost/noteful_api',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://catrinafuentes@localhost/noteful_test_api'
   }
